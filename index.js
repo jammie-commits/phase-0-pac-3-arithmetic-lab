fb8167a427e7bb889f1939708acc33305f7822e8
jammie-commits <jay.mbugua.ph@gmail.com>
2024-03-22 11:44:42 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  function divide(a, b) {
+    return a / b;
+  }
+  function increment(a) {
+    return ++a;
+  }
+  function decrement(b) {
+    return --b;
+  }
+  function parseInteger(d) {
+    return parseInt(d);
+  }
+  function makeInt(a) {
+    return parseInt(a, 10);
+  }
+  function preserveDecimal(z) {
+    return parseFloat(z);
+  }
